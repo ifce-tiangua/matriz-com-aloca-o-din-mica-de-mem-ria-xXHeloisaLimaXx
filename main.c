38585ae573b30a778d0a0df94c23e1718941aec2
xXHeloisaLimaXx <Helo.araujw@gmail.com>
2024-11-19 17:40:18 -0300

    Atualizar o main.c

--- main.c
+++ main.c
@@ -2,40 +2,52 @@
 #include <stdlib.h>
 
 int main() {
-    int **matriz;
+    int **matriz; // Ponteiro para ponteiro para armazenar a matriz
     int linha, coluna;
     int i, j;
 
-    scanf("%d", &linha);
-    scanf("%d", &coluna);
+    // Leitura do número de linhas e colunas
+    printf("Digite o número de linhas e colunas: ");
+    scanf("%d %d", &linha, &coluna);
 
+    // Alocação dinâmica da matriz (array de ponteiros)
     matriz = (int **)malloc(linha * sizeof(int *));
-    for (i = 0; i < linha; i++) {
-        matriz[i] = (int *)malloc(coluna * sizeof(int));
+    if (matriz == NULL) {
+        printf("Erro ao alocar memória para as linhas.\n");
+        return 1;
     }
 
+    // Alocação das colunas para cada linha
+    for (i = 0; i < linha; i++) {
+        matriz[i] = (int *)malloc(coluna * sizeof(int));
+        if (matriz[i] == NULL) {
+            printf("Erro ao alocar memória para a linha %d.\n", i);
+            return 1;
+        }
+    }
+
+    // Preenchendo a matriz com valores fornecidos pelo usuário
+    printf("Digite os elementos da matriz:\n");
     for (i = 0; i < linha; i++) {
         for (j = 0; j < coluna; j++) {
             scanf("%d", &matriz[i][j]);
         }
     }
 
+    // Exibindo a matriz
+    printf("Matriz:\n");
     for (i = 0; i < linha; i++) {
         for (j = 0; j < coluna; j++) {
-            printf("%d", matriz[i][j]);
-            if (j < coluna - 1) {
-                printf(" ");
-            }
+            printf("%d ", matriz[i][j]);
         }
-        if ( linha == 1) {
-            printf("\\n");
-        }
+        printf("\n");
     }
 
+    // Liberando a memória alocada
     for (i = 0; i < linha; i++) {
-        free(matriz[i]);
+        free(matriz[i]); // Libera cada linha
     }
-    free(matriz);
+    free(matriz); // Libera o array de ponteiros
 
     return 0;
 }
